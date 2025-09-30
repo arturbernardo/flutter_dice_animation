3a689253249db68fc874a1f331f330e40b0027de
arturbernardo <artur.bernardo.k@gmail.com>
2025-09-30 10:59:37 -0300

    init

---
+++ lib/dice_roller.dart
@@ -0,0 +1,108 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/styled_text.dart';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> with SingleTickerProviderStateMixin {
+  var lastRoll = 2;
+  var currentDiceRoll = 2;
+
+  late final AnimationController _controller;
+  late final Animation<double> _scaleAnimation;
+  late final Animation<double> _rotationAnimation;
+  late final Animation<Offset> _positionAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 0.5).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _rotationAnimation = Tween<double>(begin: 0, end: 2 * pi).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _positionAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(-1.0, -0.8),
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) {
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void rollDice() {
+    setState(() {
+      lastRoll = currentDiceRoll;
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.translate(
+              offset: _positionAnimation.value * 100,
+              child: Transform.rotate(
+                angle: _rotationAnimation.value,
+                child: Transform.scale(
+                  scale: _scaleAnimation.value,
+                  child: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 300),
+                    child: Image.asset(
+                      'images/dice-$currentDiceRoll.png',
+                      key: ValueKey(currentDiceRoll),
+                      width: 200,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          child: const StyledText('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/dice_roller.dart';
+
+import 'package:flutter_application_1/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+          child: DiceRoller(),
+        ),
+      ),
+    ),
+  );
+}
