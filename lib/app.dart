5c5d403e66babd11321e69ea3543e13fe115de4a
arturbernardo <artur.bernardo.k@gmail.com>
2025-10-03 22:27:53 -0300

    two pages

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/first_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Navegação com Footer',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const FirstScreen(),
+    );
+  }
+}
---
+++ lib/first_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/second_screen.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Tela Principal")),
+      body: const Center(child: Text("Você está na Tela Principal")),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const SecondScreen()),
+                );
+              },
+              child: const Text("Ir"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
